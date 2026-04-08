edc9bf57483c1f3aaadb8af3e61e6806a49b6277
Nirajan09 <nirajantiwari09@gmail.com>
2026-04-08 23:18:21 +0545

    feat: refine Landing page UI with improved styling and layout

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,3 +1,45 @@
-export default function Landing() {
-  return <h2>Landing Page</h2>;
-}
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <div
+      className="min-h-screen flex flex-col justify-center items-center text-center px-4 bg-linear-to-r from-(--color-primary-dark) to-(--color-primary)"
+    >
+      <h1 className="text-5xl font-bold text-(--color-accent-light) mb-4">
+        Welcome to Real Estate Portal
+      </h1>
+
+      <p className="text-(--color-secondary) text-lg mb-8 max-w-xl">
+        Discover your dream property and save your favorites effortlessly.
+      </p>
+
+      <div className="flex flex-col sm:flex-row gap-4">
+        {/* Login Button */}
+        <Link
+          to="/login"
+          className="bg-(--color-accent-light) text-(--color-primary-dark) font-semibold px-6 py-3 rounded-lg shadow-lg hover:bg-[#f0f0c5 transition-colors"
+        >
+          Login
+        </Link>
+
+        {/* Register Button */}
+        <Link
+          to="/register"
+          className="bg-transparent border-2 border-(--color-accent-light) text-(--color-accent-light) font-semibold px-6 py-3 rounded-lg hover:bg-(--color-accent-light) hover:text-(--color-primary-dark) transition-colors"
+        >
+          Register
+        </Link>
+
+        {/* Continue as Guest */}
+        <Link
+          to="/dashboard"
+          className="bg-(--color-primary-dark) text-(--color-accent-light) font-semibold px-6 py-3 rounded-lg shadow-lg hover:bg-(--color-primary) transition-colors"
+        >
+          Continue as Guest
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
